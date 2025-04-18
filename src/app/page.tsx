5c6b44ef7f950858bc26370dbf371c7c5b2726d1
jaredataylor <jaredalextaylor@gmail.com>
2025-04-18 12:06:58 -0400

    Debug netlify form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -578,7 +578,8 @@ const HomePage: React.FC = () => {
             <div className="flex flex-col items-center space-y-4">
               {/* <a href={`mailto:${companyInfo.email}?subject=Inquiry%20about%20Fiberglass%20Columns`} className="text-blue-600 hover:underline text-lg">{companyInfo.email}</a> */}
               <a href={`tel:${companyInfo.number}`} className="text-blue-600 hover:underline text-lg">{companyInfo.number}</a>
-              <form name="contact" netlify hidden>
+              <form name="contact" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
                 <p>
                   <label>Your Name: <input type="text" name="name" /></label>
                 </p>
