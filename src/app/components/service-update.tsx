ab0544e2e9c1a7806969c1da87b55211ba35c519
jaredataylor <jaredalextaylor@gmail.com>
2025-06-15 16:44:32 -0400

    Fix puncutation

--- src/app/components/service-update.tsx
+++ src/app/components/service-update.tsx
@@ -12,7 +12,7 @@ export function ServiceUpdate() {
                     Our expert team specializes in removing, replacing, and restoring finished fiberglass columns for residential and commercial properties. We have recently completed projects in key Ontario communities including Markham, Unionville, Stouffville, Richmond Hill, Woodbridge, Brampton, and Mississauga.
                 </p>
                 <p className="text-gray-700 pb-4 text-justify">
-                    Whether you're dealing with cracked, aging, or outdated columns, we offer reliable, high-quality service that enhances curb appeal and structural integrity. If you're located in the GTA or surrounding regions, we are ready to help with your next column replacement or renovation project.
+                    Whether you`re dealing with cracked, aging, or outdated columns, we offer reliable, high-quality service that enhances curb appeal and structural integrity. If you`re located in the GTA or surrounding regions, we are ready to help with your next column replacement or renovation project.
                 </p>
                 <p className="text-gray-700 pb-4 text-justify">
                     Contact us today to learn how we can improve the look and safety of your property with our trusted fiberglass column services.
