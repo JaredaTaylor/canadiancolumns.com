940911af0a65f346cd261f36f9ec2e6ca537016a
Jared Taylor <43914253+JaredaTaylor@users.noreply.github.com>
2025-06-16 10:47:35 -0400

    Update header order

--- src/app/components/service-update.tsx
+++ src/app/components/service-update.tsx
@@ -3,8 +3,8 @@ export function ServiceUpdate() {
     return (
         <section id="service-update" className="py-20 bg-gray-50">
             <div className="container mx-auto lg:px-20 px-6">
-                <h2 className="text-2xl font-semibold mb-4 text-center">Fiberglass Column Replacement Services Now Available Across the Greater Toronto Area</h2>
-                <h3 className="text-xl font-semibold mb-4 text-center">Now Serving Markham, Mississauga, London & More</h3>
+                <h2 className="text-2xl font-semibold mb-4 text-center">Now Serving Markham, Mississauga, London & More</h2>
+                <h3 className="text-xl font-semibold mb-4 text-center">Fiberglass Column Replacement Services Now Available Across the Greater Toronto Area</h3>
                 <p className="text-gray-700 pb-4 text-justify">
                     We are proud to expand our fiberglass column replacement and architectural column renovation services throughout the Greater Toronto Area (GTA)—now reaching as far west as London, Ontario.
                 </p>
